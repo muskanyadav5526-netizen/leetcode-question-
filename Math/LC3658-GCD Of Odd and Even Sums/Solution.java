37f61b74e85a5ece144d22f57c7a1ccf740d4648
muskanyadav5526-netizen <muskanyadav5526@gmail.com>
2026-07-16 23:12:57 +0530

    Update Solution.java

--- Math/LC3658-GCD Of Odd and Even Sums/Solution.java
+++ Math/LC3658-GCD Of Odd and Even Sums/Solution.java
@@ -1,1 +1,9 @@
-# code
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        //sum of first n odd numbers=n*n
+        //sum of first n even numbers=n*(n+1)
+        //gcd(n*n,n*(n+1))-->ngcd(n,n+1)-->n*1=n
+        // n and n+1 are coprime 
+        return n;
+    }
+}
