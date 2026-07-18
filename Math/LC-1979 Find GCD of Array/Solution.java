992b34993407ecb470fbe560feb235bb5a4e6423
muskanyadav5526-netizen <muskanyadav5526@gmail.com>
2026-07-19 00:09:03 +0530

    Update Solution.java

--- Math/LC-1979 Find GCD of Array/Solution.java
+++ Math/LC-1979 Find GCD of Array/Solution.java
@@ -1,1 +1,20 @@
-#code
+class Solution {
+    int gcd(int a,int b){
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+    public int findGCD(int[] nums) {
+       int smallest=Integer.MAX_VALUE;
+       int largest=Integer.MIN_VALUE;
+       for(int x:nums){
+            smallest=Math.min(smallest,x);
+            largest=Math.max(largest,x);
+       }
+       return gcd(smallest,largest);
+       // time complexity=O(n)+O(logM)=O(n)
+    }
+}
